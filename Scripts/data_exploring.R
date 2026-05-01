c4f1a7b4e3641c98e62457adfde661f87f9d44ca
Juliana Balluffi-Fry <38500356+jballuffi@users.noreply.github.com>
2026-05-01 18:06:21 -0400

    exploring more avenues

--- Scripts/data_exploring.R
+++ Scripts/data_exploring.R
@@ -6,6 +6,9 @@ lapply(dir('R', '*.R', full.names = TRUE), source)
 
 # Import all data ---------------------------------------------------------
 
+#predator bird data previously cleaned/prepped
+preddat <- readRDS("Output/Data/predator_abundance.rds")
+
 #list all files with trap per night in the name
 files <- dir("Input/NEON_count-small-mammals/", pattern = "mam_pertrapnight", recursive = TRUE, full.names = TRUE)
 
@@ -57,45 +60,104 @@ siteinfo <- trapdat[taxonID == "PELE" | taxonID == "PEMA",
 
 
 
-# mice data ---------------------------------------------------------------
+# get general mice densities and body size ---------------------------------------------------------------
 
 #grab instances of PELE and PEMA mice
 mice <- trapdat[taxonID == "PELE" | taxonID == "PEMA"]
 
-#get sample size + weight + foot BY (year, month, and species)
-pop <- mice[, .(N = .N, 
-                weight = mean(weight, na.rm = TRUE), 
-                foot = mean(hindfootLength, na.rm = TRUE)), 
-            by = .(y, m, taxonID)]
+#make year a factor, make month an integer
+mice[, y := as.factor(y)]
+mice[, m := as.factor(m)]
+
+#get mice abundance + weight + foot BY (year, month)
+byyear <- mice[, .(N = .N, 
+                   weight = mean(weight, na.rm = TRUE), 
+                   foot = mean(hindfootLength, na.rm = TRUE)), 
+               by = .(y, m, taxonID)]
 
 #reorder
-setorder(pop, m, y)
-
-#make year a factor, make month an integer
-pop[, y := as.factor(y)]
-pop[, m := as.factor(m)]
+setorder(byyear, taxonID, m, y)
 
 
-ggplot(pop)+
+#get sample size + weight + foot BY (year, month, and species)
+bysite <- mice[, .(N = .N, 
+                weight = mean(weight, na.rm = TRUE), 
+                foot = mean(hindfootLength, na.rm = TRUE)), 
+            by = .(siteID, y, taxonID)]
+
+
+
+
+# merge bysite with predator abundance ---------------------------------------------
+
+#change col name that is the same as Mice
+setnames(preddat, "N", "predabund")
+
+#merge pop and pred datasets
+bysite <- merge(bysite, preddat, by = c("y", "siteID"), all.x = TRUE)
+
+#anywhere with NA predators make a zero. This might be wrong
+#correct later if needed
+bysite[is.na(predabund), predabund := 0]
+
+
+
+# explore full mice dataset ----------------------------------------------------
+
+#look at body mass in september in response to latitude
+ggplot(mice[m == 9])+
+  geom_point(aes(x = decimalLatitude, y = weight, color = taxonID))+
+  geom_smooth(aes(x = decimalLatitude, y= weight, color = taxonID), method = "lm")+
+  labs(x = "Latitude", y = "Weight (g)", title = "September weights")
+
+
+ggplot(mice[adultTicksAttached == "N" | adultTicksAttached == "Y"])+
+  geom_boxplot(aes(fill = adultTicksAttached, y = weight, x = taxonID))+
+  scale_fill_discrete(name = "Ticks")+
+  facet_wrap(~m, scales = "free")
+
+ggplot(mice[sex == "F" & pregnancyStatus == "pregnant" | pregnancyStatus == "not"])+
+  geom_boxplot(aes(x = pregnancyStatus, y = weight))
+
+
+
+
+
+# explore the by year dataset --------------weighted.mean()# explore the by year dataset -----------------------------------------
+
+#mice abundance by month for each year
+ggplot(byyear)+
   geom_line(aes(x = m, y = N, group = taxonID, color = taxonID))+
   labs(x = "month", y = "sample size")+
   facet_wrap(~y)
 
-
-ggplot(pop)+
+#mice weight by month for each year
+ggplot(byyear)+
   geom_line(aes(x = m, y = weight, group = taxonID, color = taxonID))+
   labs(x = "month", y = "mean weight (g)")+
   facet_wrap(~y)
 
-
-ggplot(pop)+
+#mice foot length by month for each year
+ggplot(byyear)+
   geom_line(aes(x = m, y = foot, group = taxonID, color = taxonID))+
   labs(x = "month", y = "mean foot length (mm)")+
   facet_wrap(~y)
 
 
-ggplot(mice[m == 9])+
-  geom_point(aes(x = decimalLatitude, y = weight, color = taxonID))+
-  geom_smooth(aes(x = decimalLatitude, y= weight, color = taxonID), method = "lm")+
-  labs(x = "Latitude", y = "Weight (g)", title = "September weights")
+
+# explore the by site data set ----------------------------------------------
+
+#is there any pattern between mice abundance and predator abundance
+ggplot(bysite)+
+  geom_point(aes(x = N, y = predabund))+
+  geom_smooth(aes(x = N, y = predabund), method = "lm")+
+  facet_wrap(~taxonID)
+
+bysite[predabund > 0, predbin := "yes"]
+bysite[predabund == 0, predbin := "no"]
+
+ggplot(bysite)+
+  geom_boxplot(aes(x = predbin, y = N))
+
+
 
